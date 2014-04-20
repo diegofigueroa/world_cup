ac3abfc448ac2d21fcefdd7d7b46162854ce42a6
Diego Figueroa <diegofigueroa99@gmail.com>
2014-04-20 12:07:22 -0600

    updates to stadium serializer

--- app/models/stadium.rb
+++ app/models/stadium.rb
@@ -4,4 +4,6 @@ class Stadium < ActiveRecord::Base
   validates :capacity, presence: true
   validates :construction_year, presence: true
   validates :image_url, presence: true
+  
+  has_many :matches
 end
--- app/serializers/stadium_serializer.rb
+++ app/serializers/stadium_serializer.rb
@@ -1,7 +1,13 @@
 class StadiumSerializer < ActiveModel::Serializer
-  attributes :name, :city, :construction_year, :capacity, :image_url, :url
+  attributes :name, :city, :construction_year, :capacity, :matches, :image_url, :url
+  has_many :matches
   
   def url
     stadium_url(object)
   end
+  
+  def include_matches?
+    scope.params[:embed].try(:member?, 'matches')
+  end
+  
 end
